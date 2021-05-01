44447634483f2246f2e7b416a3ab0a757444070c
mirandalv <zhonghui.lv@gmail.com>
2021-05-01 16:18:45 -0400

    add test3

--- 3_NLPinTF/Week2/week2.py
+++ 3_NLPinTF/Week2/week2.py
@@ -1,15 +1,27 @@
 
+
+"""
+This week you looked at taking your tokenized words and using Embeddings to establish meaning from them in a mathematical way.
+Words were mapped to vectors in higher dimensional space, and the semantics of the words then learned when those words were labelled
+with similar meaning. So, for example, when looking at movie reviews, those movies with positive sentiment had the dimensionality of
+their words ending up ‘pointing’ a particular way, and those with negative sentiment pointing in a different direction.
+From this, the words in future sentences could have their ‘direction’ established, and from this the sentiment inferred.
+sYou then looked at sub word tokenization, and saw that not only do the meanings of the words matter, but also the sequence in which they are found.
+"""
+
+
 import tensorflow as tf
 print(tf.__version__)
 import numpy as np
 
 # needed for tf 1x
-tf.enable_eager_execution()
+# tf.enable_eager_execution()
 
 # pip install -q tensorflow-datasets if using
 
 import tensorflow_datasets as tfds
 
+# get the data
 imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
 
 train_data, test_data = imdb["train"], imdb["test"]
@@ -26,6 +38,79 @@ for s, l in train_data:
 for s, l in test_data:
     testing_sentences.append(str(s.numpy()))
     testing_labels.append(l.numpy())
+
+
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
+
+vocab_size = 10000
+embedding_dim = 16
+max_length = 120
+trunc_type = 'post'
+oov_tok = "<OOV>"
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+
+word_index = tokenizer.fit_on_texts(training_sentences)
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded = pad_sequences(sequences, maxlen=max_length, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length)
+
+reverse_word_index = idct([(value, key) for (key, value) in word_index.items()])
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+print(decode_review(padded[3]))
+print(training_sentences[3])
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(6, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+num_epochs = 10
+model.fit(padded, training_labels_final, epochs=num_epochs, validation_data=(testing_padded, testing_labels_final))
+
+# this is to get the embedding layer, which is the first layer of the model
+e = model.layers[0]
+weights = e.get_weights()[0] # get the weights of embedding layer
+print(weights.shape)
+
+import io
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(1, vocab_size):
+  word = reverse_word_index[word_num]
+  embeddings = weights[word_num]
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
+
+sentence = "I really think this is amazing. honest."
+sequence = tokenizer.texts_to_sequences([sentence])
+print(sequence)
+
+
+
+
+
+
+
+
 
 
 
